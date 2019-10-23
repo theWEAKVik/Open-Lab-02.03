fc79ba95dd0f4994bfccb4d3ac1252665f78e128
Erik Zemčík <zemcik.erik@gmail.com>
2019-10-23 20:52:42 +0200

    Created test

---
+++ Open-Lab-02.03/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_02._03
+{
+    public class Calculator
+    {
+        public int Remainder(int dividend, int divisor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-02.03/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Open_Lab_02._03
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private Calculator calculator;
+
+        private const int RandSeed = 1235789;
+        private const int RandTestCasesCount = 95;
+
+        [OneTimeSetUp]
+        public void Init() => calculator = new Calculator();
+
+        [TestCase(1, 3, 1)]
+        [TestCase(3, 4, 3)]
+        [TestCase(25, 5, 0)]
+        [TestCase(100, 6, 4)]
+        [TestCase(-9, 15, -9)]
+        [TestCaseSource(nameof(GetRandom))]
+        public void Remainder(int dividend, int divisor, int quotient) =>
+            Assert.AreEqual(quotient, calculator.Remainder(dividend, divisor));
+
+        private static IEnumerable GetRandom()
+        {
+            var random = new Random(RandSeed);
+
+            for (var i = 0; i < RandTestCasesCount; i++)
+            {
+                var dividend = random.Next();
+                var divisor = random.Next(dividend);
+                yield return new TestCaseData(dividend, divisor, dividend % divisor);
+            }
+        }
+
+    }
+}
